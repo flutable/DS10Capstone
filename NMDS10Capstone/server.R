3c68c4c7b67a999bc2bcd098dfdfd7de1c244641
flutable <flutable@hotmail.com>
2018-04-07 12:14:35 +1000

    Cleaned up UI, "prediction" now works reactively, removed blank output.

--- NMDS10Capstone/server.R
+++ NMDS10Capstone/server.R
@@ -9,15 +9,6 @@ library(sqldf)
 # cat(file=stderr()," message", variable)
 
 shinyServer(function(input, output) {
-  #constants
-  InsufficientInput <- "Insufficient input" #...to make a prediction; input string not long enough.
-  
-  output$textDisplay <- renderText({
-    
-    paste0(input$usertext, " ", "####")  #initially, just put a random
-     
-  })
-  
   # Utility functions ----
   getNgram <- function(userinput, nglength){
     #get the most recent nglength-gram from user input
@@ -32,24 +23,15 @@ shinyServer(function(input, output) {
     #remove all non-predictable characters from input
     return(gsub("[[:digit:]]|[[:punct:]]", "", x=inputtext, perl=FALSE))
   }
-  # GetNgramlastword <- function(ngram, n) {
-  #   #last word in split string is no.of tokens in ngram,hence "n"
-  #   ifelse(length(ngram) > 0, result <- str_split(ngram, boundary("word"))[[1]][n], 
-  #                             result <- "NoNgram")
-  #   return(result)
-  # }
-  # GetNgramfirstwords <- function(ng, n){
-  #    #Specify the ngram, and the order of the ngram eg "of the", 2  yields "of"; "of the fish",3 yields "of the"
-  #   ifelse(length(ng) > 0, result <- glue::collapse(str_split(ng, boundary("word"))[[1]][1:n-1], sep=" "), 
-  #                             result <- "NoNgram")
-  #   return(as.character(result))
-  # }
+
+  RemBlank <- function(st) {
+    # Remove blank output from being displayed
+    if (st=="character(0)" || is.na(st)) {return("")} else {return(st)}
+  }
   
   # Prediction ----
   #Create reactive conductor so we use usertext only once
    utR <- reactive({ SanitiseInput(input$usertext) })
-   inputlen <- reactive ({inputlen <- length( paste0(utR()) )})
-   #inputlen <- length( paste0(utR())) 
 
    n1 <-  reactive({      getNgram(utR(), 1)     }) #reactive necessary as input$usertext is reactive
    n2 <-  reactive({ word(getNgram(utR(), 2), 1) }) #word extracts  
@@ -57,55 +39,38 @@ shinyServer(function(input, output) {
    n4 <-  reactive({ word(getNgram(utR(), 4), 1) }) 
    n5 <-  reactive({ word(getNgram(utR(), 5), 1) })
 
-   #Have we seen this ngram before? Lookup ngram and extract it.
-
-    p2_1g   <- reactive({ ngram2[.(n1() ) ][order(-pr)][1,] })
-    p2_1    <- reactive({ p2_1g()$w2 })
-    p2      <- reactive({ stri_join(p2_1g()$w1, p2_1g()$w2, collapse= " ") }) #ngram + prediction
+    p2_1g   <- reactive({ ngram2[.(n1() ) ][order(-pr)][1,] })  #lookup n1, order by descending pr, first result only
+    p2_1    <- reactive({ RemBlank(p2_1g()$w2) })               #Prediction is the NEXT word; remove blanks for displaying in app 
+    n2stem  <- reactive({ RemBlank(p2_1g()$w1) })               #Stem of 2gram ie a word entered, not the prediction
     
     p3_2g   <- reactive({ p3_2g <- ngram3[.(n2(), n1() )][order(-pr)][1,]  })
-    p3_2    <- reactive({ p3_2g$w3 }) 
-    p3_2len <- reactive({ length(p3_2) })
+    p3_2    <- reactive({ RemBlank(p3_2g()$w3) }) 
+    n3stem  <- reactive({ stri_join(RemBlank(p3_2g()$w1), SPACE, RemBlank(p3_2g()$w2), collapse=" ") })
     
     p4_3g   <- reactive({ ngram4[.(n3(), n2(), n1() ) ][order(-pr)][1,] })
-    p4_3    <- reactive({ p4_3g$w4 })
+    p4_3    <- reactive({ RemBlank(p4_3g()$w4) })
+    n4stem <- reactive({ stri_join(RemBlank(p4_3g()$w1),  SPACE, RemBlank(p4_3g()$w2), SPACE, RemBlank(p4_3g()$w3), collapse=" ") })
     
     p5_4g   <- reactive({ ngram5[.(n4() , n3() , n2(), n1() )][order(-pr)][1,] })
-    p5_4    <- reactive({ p5_4g$w5 })
-    p5_4len <- reactive({ length( p5_4g ) })
+    p5_4    <- reactive({ RemBlank(p5_4g()$w5) })
+    n5stem  <- reactive({ stri_join(RemBlank(p5_4g()$w1), SPACE, RemBlank(p5_4g()$w2), SPACE, RemBlank(p5_4g()$w3), SPACE, RemBlank(p5_4g()$w4), collapse=" ") })
 
-    # Pure MLE method
-    # reactive({
-    #   if       ( !is.na(p5_4g()$w5 )) {prediction <- p5_4g()$w5}  #need to check na, since ngram may not exist.
-    #   else if ( !is.na(p4_3g()$w4 )) {prediction <- p4_3g()$w4}  #   smoothing will fix this
-    #   else if ( !is.na(p3_2g()$w3 )) {prediction <- p3_2g()$w3}
-    #   else if ( !is.na(p2_1g()$w2 )) {prediction <- p2_1g()$w2}
-    # })
-   # return( prediction)
-   
-  #get predictions from each of the ngrams
+   output$textDisplay <- renderText({
+       if      ( !is.na(p5_4g()$w5 )) { prediction <- p5_4() }  # p5_4g()$w5need to check na, since ngram may not exist.
+       else if ( !is.na(p4_3g()$w4 )) { prediction <- p4_3() }  # p4_3g()$w4  smoothing will fix this
+       else if ( !is.na(p3_2g()$w3 )) { prediction <- p3_2() }  #p3_2g()$w3
+       else if ( !is.na(p2_1g()$w2 )) { prediction <- p2_1() }  #p2_1g()$w2)
+    })
  
-    
-  #output$n1 <- renderText({ getNgram(input$usertext, 1) }) #must be in renderText to be reactive
-   output$n1    <- renderText({ n1()   })   #Need () as n1() is a reactive object, not just a string/ngram
-   output$p2_1  <- renderText({ p2_1() })
-   output$p2_1g <- renderText({ p2() })
- #  
-   output$n2   <- renderText({ n2() }) 
-   #output$p3_2 <- renderText({ p3_2() })
- #  
-   output$n3   <- renderText({ n3() }) 
-   #output$p4_3 <- renderText({ p4_3() })
- #  
-   output$n4   <- renderText({ n4() }) 
-  # output$p5_4 <- renderText({ p5_4() })
- #  
-   output$n5   <- renderText({ n5() })
- #  output$n6_5 <- renderText({ p6_5()})
- #  # output$inputlen <- renderText({inputlen})
- #  # output$inputlen <- renderText({inputlen})
- #  
- #  #output$p3_2len <- renderText({p3_2len()})
- # # output$p6_5len <- renderText({p6_5len()})
- # output$inputlen <- renderText({inputlen()})
+  #Need () as n1() is a reactive object, not just a string/ngram   
+   output$n1   <- renderText({ n2stem() })   #existing 1gram
+   output$p2_1 <- renderText({ p2_1()   }) #prediction with blanks removed p2_1g()$w2
+   output$n2   <- renderText({ n3stem() }) 
+   output$p3_2 <- renderText({ p3_2()   }) #prediction p3_2g()$w3
+   output$n3   <- renderText({ n4stem() }) 
+   output$p4_3 <- renderText({ p4_3()   }) #prediction   p4_3g()$w4
+   output$n4   <- renderText({ n5stem() }) 
+   output$p5_4 <- renderText({ p5_4()   }) #prediction p5_4g()$w5
+
+   #output$textDisplay <- renderText({ prediction })
 })
--- NMDS10Capstone/ui.R
+++ NMDS10Capstone/ui.R
@@ -16,13 +16,17 @@ shinyUI(fluidPage(
   tags$head(
     tags$style("#p2_1{display:inline}"),
     tags$style("#p2_1g{display:inline}"),
+    
     tags$style("#p3_2{display:inline}"),
     tags$style("#p4_3{display:inline}"),
     tags$style("#p5_4{display:inline}"),
     tags$style("#p6_5{display:inline}"),
-    tags$style("#inputlen{display:inline}"),
+
     tags$style("#p3_2len{display:inline}"),
     tags$style("#p5_4len{display:inline}"),
+    
+    tags$style("#textDisplay{display:inline}"),
+    #tags$style("#p5_4len{display:inline}"),
 
     tags$style("#n1{display:inline}"),
     tags$style("#n2{display:inline}"),
@@ -36,39 +40,42 @@ shinyUI(fluidPage(
   
   #  
   fluidRow(
-    column(9,
+  #   column(9,
+  #     textInput("usertext", 
+  #               "Enter a phrase", 
+  #               "")
+  #   ),
+  #   column(3,
+  #     p(strong("Predicted: "), textOutput("textDisplay"))
+  #   )
+  # ),  
+  column(12,
       textInput("usertext", 
                 "Enter a phrase", 
-                "")
-    ),
-    column(3,
-      p(strong("Predicted: "), textOutput("textDisplay"))
-    )
-  ),  
+                ""),
+             p(style="display:inline","The app displays the last 1-gram to 4-gram you enter."),
+             p(style="display:inline","The predicted word from each ngram length is in",strong("bold.")),
+            br(),
+             p(style="display:inline","The algorithm tries to find the prediction from the longest ngram you have entered: if a 4-gram is found in the database, the prediction is the most-likely word after the 4-gram. If a 4 gram is not found, the prediction is the most-likely word after a 3-gram, etc."),
+            br(),
+             p(style="display:inline",strong("Predicted: "), textOutput("textDisplay"))
+      
+   )),
+
   fluidRow(
     br(),
     h4(" N-gram input (right to left)"),
     column(12,
-           h6(strong("inputlen: ", style="display:inline"), textOutput("inputlen"  ) ),
-           h5(strong("1-gram: ", style="display:inline"), textOutput("n1"  ) ),
-           h5(strong("1-pred: ", style="display:inline"), textOutput("p2_1") ),
-           h5(strong("2_1g:   ", style="display:inline"), textOutput("p2_1g") ),
+
+           # p(strong("1-gram: ", style="display:inline"), textOutput("n1"  ) ),
+           # p(strong("1-pred: ", style="display:inline"), textOutput("p2_1") ),
+           # p(strong("2_1g  : ", style="display:inline"), textOutput("p2_1g") ),
            
-           h5(strong("2-gram: ", style="display:inline"), textOutput("n2")   ),
-           h5(strong("2-pred: ", style="display:inline"), textOutput("p3_2") ),
-           # h6(strong("p3_2 len: ", style="display:inline"), textOutput("p3_2len") ),           
-           
-           h5(strong("3-gram: ", style="display:inline"), textOutput("n3")   ),
-           h5(strong("3-pred: ", style="display:inline"), textOutput("p4_3") ),
-           
-           h5(strong("4-gram: ", style="display:inline"), textOutput("n4")   ),
-           h5(strong("4-pred: ", style="display:inline"), textOutput("p5_4") ),
-          #h6(strong("p5_4 len: ", style="display:inline"), textOutput("p5_4len") ),
-              
-           h5(strong("5-gram: ", style="display:inline"), textOutput("n5")   ),
-           h5(strong("5-pred: ", style="display:inline"), textOutput("p6_5") )
-           
-        
+            p(style="display:inline", "1-gram: ", textOutput("n1"), strong(textOutput("p2_1"))  ),
+            p(style="display:inline", "2-gram: ", textOutput("n2"), strong(textOutput("p3_2"))  ),
+            p(style="display:inline", "3-gram: ", textOutput("n3"), strong(textOutput("p4_3"))  ),
+            p(style="display:inline", "4-gram: ", textOutput("n4"), strong(textOutput("p5_4"))  )   #add comma if 
+
     ) #col
   ) #fluidRow 
 )
